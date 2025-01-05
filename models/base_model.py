0943c7ba08cba2a80bab2dc209c824b5fc3e3fb1
xander254 <alexkinyanjui356@gmail.com>
2025-01-05 14:49:21 +0300

    update basemodel class to work with db

--- models/base_model.py
+++ models/base_model.py
@@ -2,18 +2,20 @@
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
+from sqlalchemy import declarative_base, Column, String, DateTime
+Base = declarative_base()
 
 
 class BaseModel:
     """A base class for all hbnb models"""
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
+        storage.new(self)
+        from models import storage
         if not kwargs:
-            from models import storage
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            storage.new(self)
+            self.id = Column(String(60), unique=True, nullable=False, Primary_key=True), str(uuid.uuid4())
+            self.created_at = Column(DateTime, nullable=False, default=datetime.utcnow)
+            self.updated_at = Column(DateTime, default=datetime.utcnow, nullable=False)
         else:
             kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
@@ -41,4 +43,13 @@ class BaseModel:
                           (str(type(self)).split('.')[-1]).split('\'')[0]})
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
+        
+        key = _sa_instance_state
+        if key in dictionary.keys:
+            del dictionary[key]
         return dictionary
+        
+    def delete(self):
+        """Delete curret instance from storage"""
+        from models import storage
+        storage.delete(self)
