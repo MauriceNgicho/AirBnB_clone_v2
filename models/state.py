3169158e035f20032c5ee2f33e53410e3cf5c737
MauriceNgicho <mrcngicho@gmail.com>
2025-01-06 14:21:42 -0800

    updated state

--- models/state.py
+++ models/state.py
@@ -1,13 +1,31 @@
 #!/usr/bin/python3
-""" A State module for HBNB project """
+"""A State module for the HBNB project"""
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from models.base_model import BaseModel, Base
+import models
+from os import getenv
+
 
 class State(BaseModel, Base):
-    """ State class that contains name and links city and states class """
+    """State class that contains name and links cities and states"""
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
-    # Establis a relationship of one to many between State and City.
-    cities = relationship('City', backref='state', cascade='all, delete-orphan')
+
+    # Establish a relationship of one-to-many between State and City
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        cities = relationship('City', backref='state', cascade='all, delete-orphan')
+
+    @property
+    def cities(self):
+        """Returns a list of City instances linked to the State"""
+        if getenv('HBNB_TYPE_STORAGE') != 'db':
+            return [
+                city for city in models.storage.all("City").values()
+                if city.state_id == self.id
+            ]
+        return []
+
+
+    
 
