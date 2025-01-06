935e30945bc6fdd6cded603ad49b7dbd37162f1c
MauriceNgicho <mrcngicho@gmail.com>
2025-01-05 18:57:18 -0800

    state.py changes

--- models/state.py
+++ models/state.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
+""" A State module for HBNB project """
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String 
-
 
 class State(BaseModel, Base):
-    """ State class """
-    __tablename__ = states
+    """ State class that contains name and links city and states class """
+    __tablename__ = 'states'
     name = Column(String(128), nullable=False)
+    # Establis a relationship of one to many between State and City.
+    cities = relationship('City', backref='state', cascade='all, delete-orphan')
+
