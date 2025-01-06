856adfce1301bbf9b704f6f248798ba13fad9362
MauriceNgicho <mrcngicho@gmail.com>
2025-01-05 18:57:56 -0800

    city.py changes

--- models/city.py
+++ models/city.py
@@ -6,6 +6,6 @@ from models.base_model import BaseModel, Base
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
-    __tablename__ = cities
+    __tablename__ = 'cities'
     name = Column(String(128), nullable=False)
-    state_id = Column(String(60), nullable=False, ForeignKey(state_id))
+    state_id = Column(String(60), nullable=False, ForeignKey('states.id'))
