9f81f48d34d7cdb11fb6e69121d13ace477d3721
MauriceNgicho <mrcngicho@gmail.com>
2025-01-09 19:08:22 -0800

    AirBnB_clone

--- models/city.py
+++ models/city.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from sqlalchemy import Column, ForeignKey, PrimaryKeyConstraint, String
+from sqlalchemy import Column, ForeignKey, String
 from models.base_model import BaseModel, Base
+from sqlalchemy.orm import relationship
 
 
 class City(BaseModel, Base):
@@ -9,3 +10,6 @@ class City(BaseModel, Base):
     __tablename__ = 'cities'
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+
+
+    places = relationship("Place", backref="cities", cascade="all, delete-orphan")
