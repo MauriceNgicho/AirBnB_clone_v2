3ddeaf2bf36899d522407cf1cfab494d692327e9
MauriceNgicho <mrcngicho@gmail.com>
2025-02-09 10:53:01 -0800

    number_route

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -35,7 +35,7 @@ def py_text(text="is cool"):
 @app.route("/number/<int:n>", strict_slashes=False)
 def number(n):
     """Returns number if its an integer"""
-    return f" {n} is a number"
+    return f"{n} is a number"
 
 
 if __name__ == "__main__":
